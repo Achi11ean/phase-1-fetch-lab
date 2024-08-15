07a8ddf3b956004c7b466adf5595d8c21bae4221
Jonathen Whitford <paredes.jonathen@yahoo.com>
2024-08-15 12:46:38 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => response.json())
+    .then((listBook) => renderBooks(listBook));
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
+  const main = document.querySelector("main");
+  let total = 0;
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
     h2.innerHTML = book.name;
     main.appendChild(h2);
+    total = total + book.numberOfPages;
   });
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
